e04818f6757a42b876bff93b6340e0d3937c1c4b
bythealphabet <bythealphabet@gmail.com>
2019-07-17 17:56:01 -0400

    worked on actions and reducers:

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // bulma import for css styling
-import "./index.css"
+import "./index.css";
 import "bulma/css/bulma.css";
 
-import App from './App';
+//redux
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
 
+// redux thunk
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
 
+const store = createStore(reducers, (applyMiddleware(thunk)));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import {combineReducers} from "redux"
+
+const bam = () =>  'pam'
+
+export default combineReducers({bam: bam})
