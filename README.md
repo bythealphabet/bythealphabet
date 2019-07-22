f1f14c9341fa1671ef2f75218f8788d574baa843
bythealphabet <bythealphabet@gmail.com>
2019-07-22 16:11:25 -0400

    server'

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a simple Blog-application made with React Redux using the Nodejs and Expressjs as backed.
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from "redux"
-import articleReducer from './articleReducer'
-import authReducer from './authReducer'
-import {reducer} from 'redux-form'
-
-
-export default combineReducers({articles: articleReducer, form: reducer, auth: authReducer})
